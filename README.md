b5db8d9afa1423af0a010f473811b0186253ad8f
IamIddy <iddyiam@gmail.com>
2022-05-21 09:01:50 -0700

    added node-express Auth 0 Primer resources

--- README.md
+++ README.md
@@ -1,1 +1,4 @@
 # learning-node
+Resources:
+    - Primer on Node-Express: https://auth0.com/blog/node-js-and-typescript-tutorial-build-a-crud-api/
+    - Auth0: https://auth0.com/blog/node-js-and-typescript-tutorial-secure-an-express-api/
--- menu-api/src/index.ts
+++ menu-api/src/index.ts
@@ -1,6 +1,3 @@
-// Primer on Node-Express: https://auth0.com/blog/node-js-and-typescript-tutorial-build-a-crud-api/
-//Auth0: https://auth0.com/blog/node-js-and-typescript-tutorial-secure-an-express-api/
-
 /**
  * Required External Modules
  */
